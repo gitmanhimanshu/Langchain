d286065c90ffa44ad1929c76ea6c08a6f9844457
Himanshu Yadav <himanshuyada70@gmail.com>
2026-03-07 08:52:37 +0530

    little chnage

--- ChatModels/cheat_model_google.py
+++ ChatModels/cheat_model_google.py
@@ -5,5 +5,5 @@ load_dotenv()
 
 # Use Google Gemini 2.5 Flash
 llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
-result = llm.invoke("tell me python coode for prime number")
+result = llm.invoke("tell me java coode for prime number")
 print(result.content)
